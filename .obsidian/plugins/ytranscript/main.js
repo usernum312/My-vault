5e68836cb24e33eb77e51721d5848aebc2c695d2
usernum605 <usernum605@gmail.com>
2026-03-26 13:19:22 +0000

    Update Thu Mar 26 13:19:21 UTC 2026

--- .obsidian/plugins/ytranscript/main.js
+++ .obsidian/plugins/ytranscript/main.js
@@ -1,4 +1,4 @@
-hybjuvar __defProp = Object.defineProperty;
+var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
